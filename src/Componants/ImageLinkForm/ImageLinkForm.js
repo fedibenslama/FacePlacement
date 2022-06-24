cf869476a993def76107e90977b36a06bc894a07
Your Name <fedi.benslama@esprit.tn>
2022-06-24 03:12:51 +0100

    Linked Database

--- src/Componants/ImageLinkForm/ImageLinkForm.js
+++ src/Componants/ImageLinkForm/ImageLinkForm.js
@@ -10,7 +10,9 @@ const ImageLinkForm = ({onInputChange,onImageSubmit}) =>{
             <div className="center">
                 <div className="center form pa4 br3 shadow-5">
                     <input className="pa2 f4 w-70 center" type='tex' onChange={onInputChange}/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onImageSubmit}>Detect</button>
+                    <button 
+                    className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" 
+                    onClick={onImageSubmit}>Detect</button>
                 </div>
             </div>
         </div>
--- src/Componants/Rank/Rank.js
+++ src/Componants/Rank/Rank.js
@@ -1,15 +1,11 @@
 import React from "react";
 
-const Rank = ({name,entries}) => {
+const Rank = ({ name, entries }) => {
     return (
         <div >
             <div className="f3 white ">
                 {`${name} Your Number of entries is ${entries}`}
             </div>
-            <div className="f1 white ">
-                {"#1"}
-            </div>
-
         </div>
     )
 }
